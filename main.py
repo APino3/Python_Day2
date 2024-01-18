069e5145f67560d41b07417fcd0bb9336edd874d
[Albalicia Espino] <Albaliciae@gmail.com>
2024-01-17 18:21:44 -0600

    code

---
+++ main.py
@@ -0,0 +1,7 @@
+#Create a program to calculate the area of a circle based on its radius.
+import math
+radius = float(input("Enter the radius of a circle: "))
+
+area = math.pi * radius * radius
+
+print("Area of the Circle is :{0}".format(area))
